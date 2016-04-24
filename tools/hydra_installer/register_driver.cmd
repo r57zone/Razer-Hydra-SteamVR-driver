f28116ec8b60a746347f65a29c051e3c7ba7ea79
Andras Beck <andras.beck@betavr.com>
2016-04-24 09:43:31 +0200

    fix steam directory auto detection

--- tools/hydra_installer/register_driver.cmd
+++ tools/hydra_installer/register_driver.cmd
@@ -1,7 +1,13 @@
 @echo off
 cls
 SET mypath=%~dp0
-for /f "tokens=2*" %%A in ('REG QUERY "HKCU\Software\Valve\Steam" /v SteamPath') DO set SteamPath=%%B
+
+REM detect Steam install directory
+for /f "tokens=2*" %%A in ('reg query hku /e /c /v /f SteamPath /t REG_SZ /s 2^>^NUL') DO (
+	set SteamPath=%%B
+	goto :start
+)
+:start
 
 REM register driver
 "%SteamPath%\steamapps\common\SteamVR\bin\win32\vrpathreg.exe" adddriver "%mypath:~0,-1%\hydra"
--- tools/hydra_installer/unregister_driver.cmd
+++ tools/hydra_installer/unregister_driver.cmd
@@ -1,7 +1,16 @@
 @echo off
 cls
 SET mypath=%~dp0
-for /f "tokens=2*" %%A in ('REG QUERY "HKCU\Software\Valve\Steam" /v SteamPath') DO set SteamPath=%%B
+
+REM detect Steam install directory
+for /f "tokens=2*" %%A in ('reg query hku /e /c /v /f SteamPath /t REG_SZ /s 2^>^NUL') DO (
+	set SteamPath=%%B
+	goto :start
+)
+:start
 
 REM unregister driver
 "%SteamPath%\steamapps\common\SteamVR\bin\win32\vrpathreg.exe" removedriver "%mypath:~0,-1%\hydra"
+
+REM always return with 0 to not interrupt uninstall
+exit /b 0
